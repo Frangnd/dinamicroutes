3f21315c3061c33482c3b0276e7307d9cc024e48
Frangnd <88601717+Frangnd@users.noreply.github.com>
2023-02-13 13:30:48 -0300

    Add files via upload

---
+++ rutas dinamicas feb/router.js
@@ -0,0 +1,28 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, '', event.target.href);
+    handleLocation();
+  };
+  
+  const routes = {
+    404: '404.html',
+    '/': 'home.html',
+    '/museo': 'museo.html',
+    '/obra': 'obra.html',
+  };
+  
+  const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+  
+    const html = await fetch(route).then((response) => response.text());
+  
+    document.getElementById('main-page').innerHTML = html;
+  };
+  
+  window.onpopstate = handleLocation;
+  window.route = route;
+  
+  handleLocation();
+  
